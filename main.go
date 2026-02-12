144d3ed78219dad6abd5d11cd5413a48d4f00a79
Semseddin M. <Semseddin.Moldibi@Advocatehealth.org>
2026-02-12 12:15:24 -0500

    FEAT: Added maximum number of columns read from the CSV file

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"io"
 
 	"github.com/olekukonko/tablewriter"
 )
@@ -14,14 +15,17 @@ func main() {
 	// Parse arguments
 	setRowLine := flag.Bool("rowline", true, "Draws a line between each row")
 	header := flag.Bool("header", true, "Use the first row as the header")
+	cols := flag.Int("cols", 0, "Maximum number of columns")
 	flag.Parse()
 
 	// Check for the CSV file path argument
 	if len(flag.Args()) == 0 {
-		fmt.Println("Usage: csv2table [arguments] [csv file]")
+		fmt.Println("Usage: csv2table [rowline|header|cols] [csv file]")
 		flag.PrintDefaults()
 		os.Exit(1)
 	}
+
+	var content [][]string = make([][]string, 0, 0)
 
 	// Open the CSV file
 	file, err := os.Open(flag.Args()[0])
@@ -32,12 +36,35 @@ func main() {
 
 	// Read the CSV file
 	csvFile := csv.NewReader(file)
-	data, err := csvFile.ReadAll()
-	if err != nil {
-		log.Fatalf("Error reading CSV file: %v", err)
-	}
-	if len(data) == 0 {
-		log.Fatal("CSV file is empty")
+
+	if *cols <= 0 {
+		content, err = csvFile.ReadAll()
+		if err != nil {
+			log.Fatalf("Error reading CSV file: %v", err)
+		}
+	} else {
+		// Read the file record by record
+		for {
+			record, err := csvFile.Read()
+			if err == io.EOF {
+				break // End of File
+			}
+			if err != nil {
+				log.Fatalf("Error reading CSV file: %v", err)
+			}
+
+			// Process only up to cols
+			data := make([]string, 0, *cols)
+			for i, field := range record {
+				if i >= *cols {
+					break // Stop processing once the column limit is reached
+				}
+				data = append(data, field)
+			}
+
+			// Use the data (which has at most cols fields)
+			content = append(content, data)
+		}
 	}
 
 	table := tablewriter.NewWriter(os.Stdout)
@@ -45,7 +72,7 @@ func main() {
 	// Should the first row be used as the table header?
 	firstRow := 0
 	if *header {
-		table.SetHeader(data[0])
+		table.SetHeader(content[0])
 		firstRow = 1
 	}
 
@@ -56,7 +83,7 @@ func main() {
 	table.SetRowLine(*setRowLine)
 
 	// Append the rest of the CSV rows to the table
-	for _, row := range data[firstRow:] {
+	for _, row := range content[firstRow:] {
 		table.Append(row)
 	}
 
